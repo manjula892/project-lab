9a77e344437f9d4a7c27de0689c87af442bf3e0a
manjula892 <manjulamanjula824892@gmail.com>
2026-04-11 03:37:48 -0700

    Add files via upload

---
+++ Think & Win.py
@@ -0,0 +1,11 @@
+import random
+print("🎮 Welcome to Guess the Number Game!")
+# Random number generate
+number = random.randint(1, 10)
+# User guess
+guess = int(input("Enter a number between 1 and 10: "))
+# Check
+if guess == number:
+    print("🎉 Correct! You guessed it right!")
+else:
+    print("❌ Wrong! The number was", number)
